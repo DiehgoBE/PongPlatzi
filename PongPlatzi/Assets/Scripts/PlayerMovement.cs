62d7c85a88883c0a115c3c3850a48ce8c6b02b18
Diego Bravo <diegobraes21@gmail.com>
2020-11-11 07:45:54 -0500

    Limitar el movimiento al Eje Y y definir limite de movimiento en eje Y - Hardcode

--- PongPlatzi/Assets/Scripts/PlayerMovement.cs
+++ PongPlatzi/Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Para ver en donde se encuentra nuestra posición del mouse
+        //Deguggear para ver en donde se encuentra nuestra posición del mouse
         Debug.Log(Input.mousePosition); 
 
         //Asignar la posición del mouse (Pixel Coordenates) a la posición del transform (X,Y,Z) (World Coordenates)
@@ -29,6 +29,17 @@ public class PlayerMovement : MonoBehaviour
         //Creamos un nuevo vector3 para tener un valor especifico en X, Y y mantener el valor de Z 
         //Sino Z se colocá automaticamente en -10 lo cual hace que no se visualice en pantalla
         //Los objetos no pueden estar a la misma altura que la camara porque sino no se visualizan
-        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z); 
+        //transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
+
+        //Limitar el movimiento del personaje solo en el eje Y y mantener fijo el eje X
+        //transform.position = new Vector3(transform.position.x, mousePos.y, transform.position.z);
+
+        //Definir el limite max y min del Eje Y para evitar que se salga de la pantalla
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(mousePos.y,-3.4f,3.4f), transform.position.z);
+
+
+
+
+
     }
 }
