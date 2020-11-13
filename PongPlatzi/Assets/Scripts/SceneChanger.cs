84f0951481165fce93a646f054e8531a4655ec02
Diego Bravo <diegobraes21@gmail.com>
2020-11-13 17:45:19 -0500

    Escena de Win y Lose

---
+++ PongPlatzi/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Extensión para interactuar entre escenas
+
+public class SceneChanger : MonoBehaviour
+{
+    //Función publica para poder cargar escenas
+    public void ChangeSceneTo(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);//Cargar escena
+    }
+
+}
